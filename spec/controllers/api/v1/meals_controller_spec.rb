ed3c3fbeeed662575e7000a84917dee96081fdad
Ernesto Soberanes <netosober@gmail.com>
2015-06-11 06:51:36 -0400

    Add meals controller index

--- spec/controllers/api/v1/meals_controller_spec.rb
+++ spec/controllers/api/v1/meals_controller_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MealsController, type: :controller do
-  describe "GET" do
-    let(:meal) { FactoryGirl.create(:meal) }
-    it "get item" do
-      get :show, id: meal.id
+  describe "GET #show" do
+    before(:each) do
+      @meal = FactoryGirl.create(:salad)
+      get :show, id: @meal.id
+    end
 
+    it "responds with 200" do
       expect(response).to be_success
-      expect(json_response[:description]).to eq(meal.description)
+    end
+
+    it "get item" do
+      expect(json_response[:description]).to eq(@meal.description)
+    end
+  end
+
+  describe "GET #index" do
+    before(:each) do
+      FactoryGirl.create_list(:salad, 2)
+      FactoryGirl.create_list(:soup, 3)
+      @meals = Meal.all
+      get :index
+    end
+    it "responds with 200" do
+      expect(response).to be_success
+    end
+    it "gets all items" do
+      expect(json_response.count).to eq(@meals.count)
     end
   end
 end
--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
-  factory :meal do
+  factory :salad, class: Meal do
     description "Caesar salad"
     day { 1.day.ago }
     hour { 1.hour.ago }
     calories 250
+  end
+  factory :soup, class: Meal do
+    description "Chicken soup"
+    day { 1.week.ago }
+    hour { 1.minute.ago }
+    calories 300
   end
 end
