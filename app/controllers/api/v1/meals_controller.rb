fd82d13b1f3cc792937b1092cd421784acf11f9b
Ernesto Soberanes <netosober@gmail.com>
2015-06-14 23:05:32 -0400

    Add Meals controller create

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::MealsController < ApplicationController
   def index
-    meals = Meal.all
-    render json: meals, status: 200
+    @meals = Meal.all
+    render json: @meals, status: 200
   end
 
   def show
-    meal = Meal.find(params[:id])
-    render json: meal, status: 200
+    @meal = Meal.find(params[:id])
+    render json: @meal, status: 200
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      render json: @meal, status: 200
+    else
+      render json: @meal.errors, status: 422
+    end
+  end
+
+  private
+  def meal_params
+    params.require(:meal).permit(:description, :day, :hour, :calories)
   end
 end
--- spec/controllers/api/v1/meals_controller_spec.rb
+++ spec/controllers/api/v1/meals_controller_spec.rb
@@ -30,4 +30,41 @@ RSpec.describe Api::V1::MealsController, type: :controller do
       expect(json_response.count).to eq(@meals.count)
     end
   end
+
+  describe "POST #create" do
+    context "with valid meal" do
+      before(:each) do
+        @meal = FactoryGirl.attributes_for(:salad)
+        post :create, meal: @meal
+      end
+
+      it "responds with 200" do
+        expect(response).to be_success
+      end
+
+      it "creates new meal" do
+        lastMeal = Meal.last
+        expect(lastMeal.description).to eq(@meal[:description])
+      end
+
+      it "returns new meal" do
+        expect(json_response[:description]).to eq(@meal[:description])
+      end
+    end
+
+    context "with invalid meal" do
+      before(:each) do
+        @meal = FactoryGirl.attributes_for(:salad, description: nil )
+        post :create, meal: @meal
+      end
+
+      it "responds with 422" do
+        expect(response).not_to be_success
+      end
+
+      it "returns new meal" do
+        expect(json_response[:description]).to include("can't be blank")
+      end
+    end
+  end
 end
