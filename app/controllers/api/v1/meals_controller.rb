880c1e20ac332f9f33ed7f26f67e63434505dce0
Ernesto Soberanes <netosober@gmail.com>
2015-06-15 00:49:22 -0400

    Add Meals controllers puts update

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::MealsController < ApplicationController
+  before_action :get_meal, only: [:show, :update]
+
   def index
     @meals = Meal.all
     render json: @meals, status: 200
   end
 
   def show
-    @meal = Meal.find(params[:id])
     render json: @meal, status: 200
   end
 
@@ -18,7 +19,20 @@ class Api::V1::MealsController < ApplicationController
     end
   end
 
+  def update
+    if @meal.update(meal_params)
+      render json: @meal, status: 200
+    else
+      render json: @meal.errors, status: 422
+    end
+  end
+
   private
+
+  def get_meal
+    @meal = Meal.find(params[:id])
+  end
+
   def meal_params
     params.require(:meal).permit(:description, :day, :hour, :calories)
   end
--- spec/controllers/api/v1/meals_controller_spec.rb
+++ spec/controllers/api/v1/meals_controller_spec.rb
@@ -34,8 +34,8 @@ RSpec.describe Api::V1::MealsController, type: :controller do
   describe "POST #create" do
     context "with valid meal" do
       before(:each) do
-        @meal = FactoryGirl.attributes_for(:salad)
-        post :create, meal: @meal
+        @meal_attr = FactoryGirl.attributes_for(:salad)
+        post :create, meal: @meal_attr
       end
 
       it "responds with 200" do
@@ -44,27 +44,72 @@ RSpec.describe Api::V1::MealsController, type: :controller do
 
       it "creates new meal" do
         lastMeal = Meal.last
-        expect(lastMeal.description).to eq(@meal[:description])
+        expect(lastMeal.description).to eq(@meal_attr[:description])
       end
 
       it "returns new meal" do
-        expect(json_response[:description]).to eq(@meal[:description])
+        expect(json_response[:description]).to eq(@meal_attr[:description])
       end
     end
 
     context "with invalid meal" do
       before(:each) do
-        @meal = FactoryGirl.attributes_for(:salad, description: nil )
-        post :create, meal: @meal
+        @meal_attr = FactoryGirl.attributes_for(:salad, description: nil )
+        post :create, meal: @meal_attr
       end
 
       it "responds with 422" do
         expect(response).not_to be_success
       end
 
-      it "returns new meal" do
+      it "returns error" do
         expect(json_response[:description]).to include("can't be blank")
       end
     end
+  end
+
+  describe "PUT #update" do
+    context "with valid meal" do
+      before(:each) do
+        @meal = FactoryGirl.create(:salad)
+        @meal_attr = FactoryGirl.attributes_for(:salad, calories: 2000 )
+        put :update, :id => @meal.id, :meal=> @meal_attr
+        @meal.reload
+      end
+
+      it "responds with 200" do
+        expect(response).to be_success
+      end
+
+      it "updates meal" do
+        expect(@meal.calories).to eq(@meal_attr[:calories])
+      end
+
+      it "returns updated meal" do
+        expect(json_response[:calories]).to eq(@meal_attr[:calories])
+      end
+    end
+
+    context "with invalid meal" do
+      before(:each) do
+        @meal = FactoryGirl.create(:salad)
+        @meal_attr = FactoryGirl.attributes_for(:salad, calories: nil )
+        put :update, :id => @meal.id, :meal=> @meal_attr
+        @meal.reload
+      end
+
+      it "responds with 422" do
+        expect(response).not_to be_success
+      end
+
+      it "doesn't updates meal" do
+        expect(@meal.calories).not_to eq(nil)
+      end
+
+      it "returns error" do
+        expect(json_response[:calories]).to include("can't be blank")
+      end
+    end
+
   end
 end
