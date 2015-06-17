e807394874775566d5b23dfb062cc69f03f15b60
Ernesto Soberanes <netosober@gmail.com>
2015-06-16 20:28:19 -0400

    Add index & show routes to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @user = current_user
+    if current_user.role == 'normal'
+      @user = current_user
+      render json: @user, status: 200
+    elsif current_user.role == 'admin' || current_user.role == 'manager'
+      @users = User.all
+      render json: @users, status: 200
+    else
+      render status: 401
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
     render json: @user, status: 200
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,28 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-
-  include Warden::Test::Helpers
-
   describe "GET #index" do
     before(:each) do
       @normalUser = FactoryGirl.create(:normalUser)
       @managerUser = FactoryGirl.create(:managerUser)
       @adminUser = FactoryGirl.create(:adminUser)
-        login_as(@normalUser, :scope => :user)
     end
     context "with normal user" do
       before(:each) do
+        sign_in :user, @normalUser
         get :index
       end
-
       it "responds with 200" do
         expect(response.status).to be 200
       end
-
       it "gets only current user" do
         expect(json_response[:email]).to eq(@normalUser.email)
       end
+      after(:each) do
+        sign_out :user
+      end
+    end
+    context "with manager user" do
+      before(:each) do
+        sign_in :user, @managerUser
+        get :index
+      end
+      it "responds with 200" do
+        expect(response.status).to be 200
+      end
+      it "gets all users" do
+        expect(json_response.count).to eq(User.count)
+      end
+      after(:each) do
+        sign_out :user
+      end
+    end
+    context "with admin user" do
+      before(:each) do
+        sign_in :user, @adminUser
+        get :index
+      end
+      it "responds with 200" do
+        expect(response.status).to be 200
+      end
+      it "gets all users" do
+        expect(json_response.count).to eq(User.count)
+      end
+      after(:each) do
+        sign_out :user
+      end
+    end
+  end
+  describe "GET #show" do
+    before(:each) do
+      @normalUser = FactoryGirl.create(:normalUser)
+      @managerUser = FactoryGirl.create(:managerUser)
+    end
+    before(:each) do
+      sign_in :user, @normalUser
+      get :show, {id: @managerUser }
+    end
+    it "responds with 200" do
+      expect(response.status).to be 200
+    end
+    it "gets only current user" do
+      expect(json_response[:email]).to eq(@managerUser.email)
+    end
+    after(:each) do
+      sign_out :user
     end
   end
 end
