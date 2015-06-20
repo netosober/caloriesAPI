066951f563b11b20b302021966ba38aa733c74ca
Ernesto Soberanes <netosober@gmail.com>
2015-06-20 15:56:26 -0500

    Correcte specs using should instead of expect

--- spec/controllers/api/v1/registrations_controller_spec.rb
+++ spec/controllers/api/v1/registrations_controller_spec.rb
@@ -9,14 +9,14 @@ describe Api::V1::RegistrationsController do
 
     it "should create the user" do
       post :create, {:user => @credentials}, :format => :json
-      response.code.should eq("200")
+      expect(response.code).to eq("200")
     end
 
     it "should return error message if the passwords are not matching" do
       @credentials[:password] = "wrong password"
       post :create, {'user' => @credentials}, :format => :json
-      response.code.should eq("401")
-      response.body['password_confirmation'].should_not be_nil
+      expect(response.code).to eq("401")
+      expect(response.body['password_confirmation']).not_to be_nil
     end
   end
 
@@ -29,20 +29,20 @@ describe Api::V1::RegistrationsController do
 
     it "should change the password" do
       put :update, {'user' => {'email' => 'asd@def.com', 'password' => 'new_password', 'password_confirmation' => 'new_password', :current_password => 'password'}, :auth_token => @user.authentication_token}, :format => :json
-      response.code.should eq("200")
+      expect(response.code).to eq("200")
     end
 
     it "should return error message if the passwords are not matching" do
       @credentials[:password] = "wrong password"
       put :update, {'user' => @credentials, :auth_token => @user.authentication_token}, :format => :json
-      response.code.should eq("401")
-      response.body['password_confirmation'].should_not be_nil
+      expect(response.code).to eq("401")
+      expect(response.body['password_confirmation']).not_to be_nil
     end
 
     it "should return error message if the token is not passed" do
       put :update, {'user' => @credentials}, :format => :json
-      response.code.should eq("401")
-      response.body['errors'].should_not be_nil
+      expect(response.code).to eq("401")
+      expect(response.body['errors']).not_to be_nil
     end
   end
 end
--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -11,17 +11,17 @@ describe Api::V1::SessionsController do
     describe "sign in" do
       it "should sign in successfully and return authentication token" do
         post :create, {'user' => @credentials}, :format => :json
-        response.code.should eq("200")
-        controller.current_user.should_not be_nil
-        controller.should be_signed_in
-        response.body['token'].should_not be_nil
+        expect(response.code).to eq("200")
+        expect(controller.current_user).not_to be_nil
+        expect(controller).to be_signed_in
+        expect(response.body['token']).not_to be_nil
       end
 
       it "should return authentication failure message" do
         post :create, {'user' => {:email => 'asd@sad.com', :password => 'asd'}}, :format => :json
-        response.code.should eq("401")
-        controller.current_user.should be_nil
-        controller.should_not be_signed_in
+        expect(response.code).to eq("401")
+        expect(controller.current_user).to be_nil
+        expect(controller).not_to be_signed_in
       end
     end
 
@@ -29,16 +29,16 @@ describe Api::V1::SessionsController do
       it "should signout the user successfully" do
         sign_in @user
         delete :destroy, {:auth_token => @user.authentication_token}, :format => :json
-        controller.current_user.should be_nil
-        controller.should_not be_signed_in
-        response.code.should eq("200")
+        expect(controller.current_user).to be_nil
+        expect(controller).not_to be_signed_in
+        expect(response.code).to eq("200")
       end
 
       it "should return error message if the toekn is not passed" do
         sign_in @user
         delete :destroy, :format => :json
-        response.code.should eq("401")
-        response.body['errors'].should_not be_nil
+        expect(response.code).to eq("401")
+        expect(response.body['errors']).not_to be_nil
       end
     end
 
